a6ceaaf80f78bef32c0dc564a950184196de0f7d
John <joedw1978@gmail.com>
2018-09-19 13:43:37 -0400

    changes

--- client/src/app/components/board/board.component.ts
+++ client/src/app/components/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SocketService } from '../../shared/socket.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-board',
@@ -8,11 +9,36 @@ import { SocketService } from '../../shared/socket.service';
 export class BoardComponent {
 	socket: any;
 	teams;
-	constructor() {
+	beerForm: FormGroup;
+	res;
+	constructor(private _fb: FormBuilder) {}
+	ngOnInit() {
 		this.socket = SocketService.getInstance();
 		this.socket.on('send teams', teams => {
 			this.teams = teams;
 		});
+
+		this.socket.on('post team response', res => {
+			this.res = res;
+		});
+		this._createForm();
 		this.socket.emit('get teams');
+	}
+
+	private _createForm() {
+		this.beerForm = this._fb.group({
+			name: [''],
+			p1: [''],
+			p2: [''],
+			score: [0]
+		});
+	}
+	addTeam() {
+		let team = {
+			name: this.beerForm.get('name').value,
+			p1: this.beerForm.get('p1').value,
+			p2: this.beerForm.get('p2').value
+		};
+		this.socket.emit('post team', team);
 	}
 }
